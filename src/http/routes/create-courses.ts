d9da238f0eef7212743aec3997aa356ffad39f95
Laize Ferraz <laizeferraz@gmail.com>
2025-08-27 18:21:42 +1000

    add zod to the routes

--- src/http/routes/create-courses.ts
+++ src/http/routes/create-courses.ts
@@ -1,25 +1,28 @@
-import { server } from "../../server.ts"
+import { type FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { courses } from "../../db/schema.ts"
 import { db } from "../../db/client.ts"
+import z from "zod/v4"
 
-export function createCourse() {
-  server.post('/courses', async(request, reply) => {
-  type Body = {
-    title: string
-  }
+export const createCourseRoute: FastifyPluginAsyncZod = async (server) => {
+  server.post('/courses', {
+    schema: {
+      tags: ['courses'],
+      summary: 'Create a course',
+      description: 'This route receives a title and create a new course in the database.',
+      body: z.object({
+        title: z.string().min(5, 'Title must have at least 5 chararacters.')
+      }),
+      response: {
+        201: z.object({courseId: z.uuid()}).describe('Course successfully created!') //to bocumented on swagger
+      }
+    }
+  }, async(request, reply) => {
 
-  const body = request.body as Body
-  const courseTitle = body.title
-
-  if (!courseTitle) {
-    return reply.status(400).send({ message: 'Title is mandatory.' })
-  }
+  const courseTitle = request.body.title
 
   const result = await db
   .insert(courses)
-  .values({
-    title: courseTitle
-  })
+  .values({title: courseTitle})
   .returning()
   return reply.status(201).send({ courseId: result[0].id})
 })
--- src/http/routes/get-courses.ts
+++ src/http/routes/get-courses.ts
@@ -1,9 +1,26 @@
+import { type FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../../db/client.ts"
 import { courses } from "../../db/schema.ts"
-import {server } from "../../server.ts"
+import { title } from "process";
+import z from "zod/v4";
 
-export function getCoursesList(){
-  server.get('/courses', async(request, reply) => {
+export const getCoursesListRoute: FastifyPluginAsyncZod = async (server) => {
+  server.get('/courses', {
+    schema: {
+      tags: ['courses'],
+      summary: 'Get all courses',
+      description: 'This route gets a list of all courses.',
+      response: {
+        200: z.object({
+          courses: z.array(
+            z.object({
+              id: z.uuid(),
+              title: z.string()
+          }))
+        })
+      }
+    }
+  }, async(request, reply) => {
     const result = await db.select({
       id: courses.id,
       title: courses.title
