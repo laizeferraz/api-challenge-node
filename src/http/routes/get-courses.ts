5c85ed9e8c81a7aa3d11ce60792a1ed9024b6209
Laize Ferraz <laizeferraz@gmail.com>
2025-08-28 18:56:41 +1000

    add filters and pagination

--- src/http/routes/get-courses.ts
+++ src/http/routes/get-courses.ts
@@ -1,8 +1,8 @@
 import { type FastifyPluginAsyncZod } from "fastify-type-provider-zod";
 import { db } from "../../db/client.ts"
-import { courses } from "../../db/schema.ts"
-import { title } from "process";
+import { courses, enrollments } from "../../db/schema.ts"
 import z from "zod/v4";
+import { and, asc, count, eq, ilike, SQL } from "drizzle-orm";
 
 export const getCoursesListRoute: FastifyPluginAsyncZod = async (server) => {
   server.get('/courses', {
@@ -10,21 +10,49 @@ export const getCoursesListRoute: FastifyPluginAsyncZod = async (server) => {
       tags: ['courses'],
       summary: 'Get all courses',
       description: 'This route gets a list of all courses.',
+      querystring: z.object({
+        search: z.string().optional(),
+        orderBy: z.enum(['id', 'title']).optional().default('id'),
+        page: z.coerce.number().optional().default(1) //everything in an URL is a string. That's the reason for using coerce here.
+      }),
       response: {
         200: z.object({
           courses: z.array(
             z.object({
               id: z.uuid(),
-              title: z.string()
-          }))
+              title: z.string(),
+              enrollments: z.number()
+          })),
+          total: z.number(),
         })
       }
     }
   }, async(request, reply) => {
-    const result = await db.select({
-      id: courses.id,
-      title: courses.title
-    }).from(courses) 
-    return reply.send({ courses: result })
+    const { search, orderBy, page } = request.query
+
+    const conditions: SQL[] = []
+
+    if(search) {
+      conditions.push(ilike(courses.title, `%${search}%`)) //ilike is not case sensitive and where accepts undefined. The % is used to search for a specific word. It can be in the end (search for the word that starts with it) or in the begining (search for the word that ends with it). 
+    }
+
+    const [result, total] = await Promise.all([
+      db
+        .select({
+        id: courses.id,
+        title: courses.title,
+        enrollments: count(enrollments.id)
+        })
+        .from(courses)
+        .leftJoin(enrollments, eq(enrollments.courseId, courses.id))
+        .orderBy(asc(courses[orderBy]))
+        .offset((page - 1 ) * 2)
+        .limit(10)
+        .where(and(...conditions))
+        .groupBy(courses.id),
+
+      db.$count(courses, and(...conditions))//for the frontend to display how many pages/items we have. It works as a metadata. The AND to guarantee all conditions are satisfied.
+    ])
+    return reply.send({ courses: result, total })
   })
 }
