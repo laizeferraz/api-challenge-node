21f7c92e18486f638237b292baad554567cb5b52
Laize Ferraz <laizeferraz@gmail.com>
2025-09-01 19:40:50 +1000

    create login test

---
+++ src/http/routes/login.test.ts
@@ -0,0 +1,26 @@
+import { test, expect } from 'vitest'
+import request from 'supertest'
+import { server } from '../../app.ts'
+import { makeUser } from '../../tests/factories/make-user.ts'
+
+test('login', async() => {
+
+  await server.ready()
+
+  const {user, passwordBeforeHash} = await makeUser()
+
+  const response = await request(server.server)
+    .post(`/login`)
+    .set('Content-Type', 'application/json')
+    .send({
+      email: user.email,
+      password: passwordBeforeHash
+    })
+  
+  expect(response.status).toEqual(200)
+  expect(response.body).toEqual({
+    message: 'ok'
+  })
+})
+
+//todo: test errors - password or email wrong - lines 32-33, 40-41
